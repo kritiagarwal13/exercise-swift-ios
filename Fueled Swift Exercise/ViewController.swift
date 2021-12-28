d77f41d84562c544995bd33488400356636d9a88
Kriti Agarwal <kriti.agarwal4709@gmail.com>
2021-12-28 17:58:24 +0530

    Initial Commit

---
+++ Fueled Swift Exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fueled Swift Exercise
+//
+//  Created by Kriti  Agarwal on 28/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
